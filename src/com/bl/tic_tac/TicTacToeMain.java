82a7b8a952111555c00aa5ce881a5edea65f412c
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-17 22:32:35 +0530

    Welcome msg aadded

---
+++ src/com/bl/tic_tac/TicTacToeMain.java
@@ -0,0 +1,10 @@
+package com.bl.tic_tac;
+
+ public class TicTacToeMain {
+        public static void main(String[] args) {
+            System.out.println("Welcome to Tic Tac Toe Game Simulation");
+            TicTacToeGame game = new TicTacToeGame();
+            game.displayBoard();
+        }
+
+}
