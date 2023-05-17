a528f5421c518be6f83986bb36147873f5196355
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-17 22:34:11 +0530

    Board Created

--- src/com/bl/tic_tac/TicTacToeMain.java
+++ src/com/bl/tic_tac/TicTacToeMain.java
@@ -3,8 +3,7 @@ package com.bl.tic_tac;
  public class TicTacToeMain {
         public static void main(String[] args) {
             System.out.println("Welcome to Tic Tac Toe Game Simulation");
-            TicTacToeGame game = new TicTacToeGame();
-            game.displayBoard();
+
         }
 
 }
